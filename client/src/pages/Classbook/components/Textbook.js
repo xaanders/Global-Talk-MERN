6a6536d125eb06461a373c783b4c2628a3030a80
xaanders <riazantsev.aleks@gmail.com>
2023-04-02 17:54:23 -0400

    fix: centering spinner

--- client/src/pages/Classbook/components/Textbook.js
+++ client/src/pages/Classbook/components/Textbook.js
@@ -10,7 +10,7 @@ function Textbook({ words }) {
     const id = useSelector(state => state.user.userInfo && state.user.userInfo.profile._id);
     const dictionary = useSelector(state => state.user.userInfo.profile.dictionary);
     const dispatch = useDispatch();
-    
+
     const addWordToDictionary = (word) => {
         if (id) {
             const newProfile = {
@@ -22,10 +22,13 @@ function Textbook({ words }) {
             dispatch(userActions.setDictionary(word.translation));
         }
     }
-    
+
     return (
         <>
-            {words.length === 0 ? <Spinner animation="border" className="spinner" role="status" style={{ width: '100px', height: '100px' }} /> :
+            {words.length === 0 ?
+                <div className="d-flex justify-content-center my-5">
+                    <Spinner animation="border" className="spinner" role="status" style={{ width: '100px', height: '100px' }} />
+                </div> :
                 <ClassbookCards words={words} wordHandler={addWordToDictionary} />}
         </>
     )
